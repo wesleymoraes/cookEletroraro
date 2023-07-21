29cd32da1bc114b2bbcd0e6e6981df48afe7ba45
betovawn1 <sergioluizfrancajr@gmail.com>
2023-07-21 17:39:50 -0300

    .

--- react/Components/GlobalStyles.jsx
+++ react/Components/GlobalStyles.jsx
@@ -1,6 +1,23 @@
-import react from 'react';
+import react, {
+    useLayoutEffect
+} from 'react';
 import styles from './GlobalStyles.css';
 
 export default function GlobalStyles() {
+
+    useLayoutEffect(() => {(function() {
+        window._callbackSettings = {
+          id: '5RKSwY'
+        };
+      
+        (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            js = d.createElement(s); js.id = '5RKSwY';
+            js.src = 'https://assets.callnow.com.br/widget.js';
+            fjs.parentNode.insertBefore(js, fjs);
+        })(document, 'script', '5RKSwY');
+      }());
+    }, [])
+
     return <div className={styles.pixelStyle}></div>;
 }
